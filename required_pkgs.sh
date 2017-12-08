661e93eacdefc7eb45ab9e57aa3b117872171547
Sundar Nagarajan <sun.nagarajan@gmail.com>
2017-12-08 09:00:59 -0800

    Add python-pexpect to required_pkgs.sh

--- required_pkgs.sh
+++ required_pkgs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-REQD_PKGS="util-linux git build-essential fakeroot libncurses5-dev libssl-dev ccache libfile-fcntllock-perl curl"
+REQD_PKGS="util-linux git build-essential fakeroot libncurses5-dev libssl-dev ccache libfile-fcntllock-perl curl python-pexpect"
 
 $(dirname $0)/pkgs_missing_from.sh $REQD_PKGS
